fe9e8d82ad0f9795901e0486bc0c64b44cfc8637
Joe Reis <josephreis@gmail.com>
2015-03-09 06:11:55 -0600

    First string problem in Swift. Super easy, but getting syntax right is the lesson.

--- Array01.playground/section-1.swift
+++ Array01.playground/section-1.swift
@@ -24,7 +24,7 @@ extension Array {
     
 }
 
-// #1 FirstLast6
+// FirstLast6
 // Given an array of ints, return True if 6 appears as either the first or last element in the array. The array will be length 1 or more.
 
 func firstLast6(nums: [Int]) -> Bool {
@@ -40,7 +40,7 @@ firstLast6([1,2,6]) //true
 firstLast6([6,1,2,3]) //true
 firstLast6([13,6,1,2,3]) //false
 
-// #2 CommonEnd
+// CommonEnd
 // Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
 
 func commonEnd(a: [Int], b: [Int]) -> Bool {
@@ -50,3 +50,12 @@ func commonEnd(a: [Int], b: [Int]) -> Bool {
 commonEnd([1,2,3], [7,3]) //true
 commonEnd([1,2,3], [7,3,2]) //false
 commonEnd([1,2,3], [1,3]) //true
+
+// MiddleWay
+// Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+func middleWay(a:[Int], b: [Int]) {
+    var newArr = [Int]()
+    newArr =
+}
+
---
+++ Strings01.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Strings01 from codingbat.com
+
+// HelloName
+// Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+func helloName(name: String) -> String {
+    return "Hello \(name)!"
+}
+
+helloName("Bob") //Hello Bob!
+
+
