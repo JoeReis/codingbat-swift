15c2bc6c2dcb8e3e4ba6c5c90162cdc6534c481e
Joe Reis <josephreis@gmail.com>
2015-03-22 05:40:08 -0600

    Substring problem. On a sidetone, Swift's way of doing substrings could improve in terms of ease and verbosity.

--- Strings01.playground/section-1.swift
+++ Strings01.playground/section-1.swift
@@ -9,4 +9,22 @@ func helloName(name: String) -> String {
 
 helloName("Bob") //Hello Bob!
 
+// MakeOutWord
+//Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". 
+
+func makeOutWord(out: String, word: String) -> String {
+    let outFirst = out[out.startIndex ..< advance(out.startIndex, 2)]
+    
+    let outLastIndex = advance(out.startIndex, 2)
+    let outLast = out[outLastIndex ..< advance(outLastIndex, 2)]
+    
+    return outFirst + word + outLast
+    
+}
+
+
+makeOutWord("<<>>", "Yay") //<<Yay>>
+makeOutWord("[[]]", "word") //[[word]]
+
+
 
